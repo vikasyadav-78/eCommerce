650c5618d9ad766afab7dccf013a3fcdb501a066
yadavvikas787840@gmail.com <yadavvikas787840@gmail.com>
2025-02-26 14:01:42 +0530

    changes for otp

--- JS/Signup.js
+++ JS/Signup.js
@@ -1,6 +1,8 @@
 function getInputNumberFrom() {
     let input = document.getElementById("input-number");
     let value = input.value;
+    let password_input = document.getElementById("password-input");
+    let password_value = password_input.value;
 
     if (value.length != 10) {
         input.classList.remove("form-control");
@@ -13,14 +15,41 @@ function getInputNumberFrom() {
         input.classList.remove("form-control")
         input.focus();
         input.classList.add("form-control-after");
-    } else {
-        input.classList.remove("form-control-after");
-        input.classList.add("form-control-active")
     }
+    else if (password_value === "") {
+        alert("Please enter a secure password");
+    } else if (password_value.length <= 8) {
+        alert("Password must be 8 characters minimum and strong password")
+    } else if(password_value.length>=8){
+        alert("Get OTP")
+    }else {
+       input.classList.remove("form-control-after");
+       input.classList.add("form-control-active")
+   }
+
 };
 
-let form  = document.getElementById("signin-form");
-function sumbitForm(event){
+let form = document.getElementById("signin-form");
+function sumbitForm(event) {
     event.preventDefault();
 }
-form.addEventListener('submit' , sumbitForm)
+form.addEventListener('submit', sumbitForm)
+
+
+function showPassword(){
+    let password_Show = document.querySelector(".password-Show");
+
+    password_Show.addEventListener('click' , () =>{
+        let passwordInput = document.getElementById("password-input").type = 'text';
+    })
+    hidePassword()
+}
+
+function hidePassword(){
+    let hide_password = document.getElementById("password-input").type = 'password'
+
+    hide_password.addEventListener("click" , () =>{
+        let password_Show = document.querySelector(".password-Show")
+        password_Show.classList.remove()  
+    })
+}
